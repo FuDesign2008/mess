9ac05a7afbc1f94c75daa9ecb927592efd7617b5
fudesign2008 <fudesign2008@163.com>
2015-10-10 15:34:10 +0800

    new file:   alg/excel-index.js

---
+++ alg/excel-index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+'use strict';
+
+/**
+ * @param {Integer} index
+ * @return {String}
+ */
+var convertIndex = function (index) {
+
+  if (index < 1) {
+    return;
+  }
+
+  var strIndex = '',
+      charCodeA = 'A'.charCodeAt(0),
+      temp;
+
+  while(index >= 1) {
+    temp = index % 26;
+    if (temp === 0) {
+      strIndex = 'Z' + strIndex;
+      index = Math.floor(index / 26) - 1;
+    } else {
+      strIndex = String.fromCharCode(temp + charCodeA - 1) + strIndex;
+      index = Math.floor(index / 26);
+    }
+  }
+
+  return strIndex;
+};
+
+
+// test
+// J
+console.log(convertIndex(10));
+
+// BC
+console.log(convertIndex(55));
+
+// YZ
+console.log(convertIndex(676));
+
+// YY
+console.log(convertIndex(675));
+
+// ZA
+console.log(convertIndex(677));
