c246084cda4ddbcf2305254926f76156a50dc4b3
fudesign2008 <fudesign2008@163.com>
2015-12-02 21:29:52 +0800

    modified:   php/w3school/user-info/get-user-info.php         modified:   php/w3school/user-info/main.js

--- php/w3school/user-info/get-user-info.php
+++ php/w3school/user-info/get-user-info.php
@@ -2,26 +2,48 @@
 
 $connect = mysql_connect('localhost', 'sqltest', 'sql_TEST_123');
 
-function responseBlank($msg) {
-    $blank = array('msg' => $msg, 'userInfo' => '');
-    echo json_encode($blank);
+function setXmlHeader() {
+    header('Content-Type: text/xml');
+    header('Cache-Control: no-cache, must-revalidate');
+    # a date in the past
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+}
+
+function echoXmlDeclaration() {
+    echo '<?xml version="1.0" encoding="utf-8" ?>';
+}
+
+function responseBlank($msg, $isXml) {
+    if ($isXml) {
+        setXmlHeader();
+        echoXmlDeclaration();
+        echo "<data>";
+        echo "<msg>$msg</msg>";
+        echo "</data>";
+    } else {
+        $blank = array('msg' => $msg, 'userInfo' => '');
+        echo json_encode($blank);
+    }
 }
 
 if (!$connect) {
     responseBlank('Failed to connect to database');
     return;
 }
+$isXml = trim($_GET['isXml']);
+$isXml = $isXml == 'true';
 
 $q = trim($_GET['q']);
 
+
 if ($q == '') {
-    responseBlank('The q is blank');
+    responseBlank('The q is blank', $isXml);
     return;
 }
 
 $arr = preg_split('/\s+/', $q);
 if (sizeOf($arr) != 2)  {
-    responseBlank('no firstName or lastName');
+    responseBlank('no firstName or lastName', $isXml);
     return;
 }
 
@@ -43,14 +65,27 @@ if ($row != NULL) {
     }
 }
 
-$response = array(
-    'userInfo' => $userInfo,
-);
+if ($isXml) {
+    setXmlHeader();
+    echoXmlDeclaration();
 
-echo json_encode($response);
+    echo "<data>";
+    echo "<userInfo>";
 
+    foreach($userInfo as $key=>$value) {
+        echo "<$key>$value</$key>";
+    }
 
+    echo "</userInfo>";
+    echo "</data>";
 
+} else {
+    $response = array(
+        'userInfo' => $userInfo,
+    );
+
+    echo json_encode($response);
+}
 
 
 
--- php/w3school/user-info/main.js
+++ php/w3school/user-info/main.js
@@ -11,11 +11,39 @@
 $(function () {
     'use strict';
 
-    var URL = './get-user-info.php',
+    var isXml = Math.random() > 0.5,
+    //var isXml = true,
+        URL = './get-user-info.php',
         $hint = $('#hint'),
         updateHint = function (html) {
             $hint.html(html);
-        };
+        },
+        handleJsonData = function (data) {
+            if (data && data.userInfo) {
+                var html = _.map(data.userInfo, function (value, key) {
+                        return key + ': ' + value;
+                    });
+                html = '<p>' + html.join('</p><p>') + '</p>';
+                updateHint(html);
+            } else {
+                updateHint('');
+            }
+        },
+        handleXmlData = function (data) {
+            var html = [];
+
+            updateHint('');
+            $(data).find('userInfo').children().each(function () {
+                html.push(this.tagName + ': ' + $(this).text());
+            });
+
+            html = '<p>' + html.join('</p><p>') + '</p>';
+            updateHint(html);
+        },
+        dataType = isXml ? 'xml' : 'json',
+        handleData = isXml ? handleXmlData : handleJsonData;
+
+    window.console.log('is xml: ' + isXml);
 
     $('#users').change(function () {
         var $this = $(this),
@@ -26,21 +54,11 @@ $(function () {
             url: URL,
             data: {
                 q: value,
+                isXml: isXml,
                 sid: _.uniqueId()
             },
-            dataType: 'json'
-        }).done(function (data) {
-            if (data && data.userInfo) {
-                var html = _.map(data.userInfo, function (value, key) {
-                        return key + ': ' + value;
-                    });
-                html = '<p>' + html.join('</p><p>') + '</p>';
-                updateHint(html);
-            } else {
-                updateHint('');
-            }
-
-        }).fail(function () {
+            dataType: dataType
+        }).done(handleData).fail(function () {
             updateHint('');
         });
 
